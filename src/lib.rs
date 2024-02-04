c29b078c1c22e1d984e3aebac805ade0afbfef2b
Matthew <matthewblake0625@gmail.com>
2024-02-04 15:07:27 -0500

    worked out how to do insert() without making it hard to keep generics.

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,54 @@
-use std::{collections::HashMap, result};
+use std::collections::HashMap;
+
+pub trait Position {
+    fn position(&self) -> (f32, f32);
+}
 
 #[derive(Debug)]
 pub struct QuadTreeInsertError;
 
 #[derive(Debug, Default, Clone)]
-pub struct QuadTree<T> {
+pub struct QuadTree<T>
+where
+    T: Position,
+{
     pub nodes: HashMap<u64, T>,
     handle_counter: u64,
-
-    min_size: f32,
 
     tree: QuadTreeInner,
 }
 
-impl<T> QuadTree<T> {
-    pub fn new(max_nodes: usize, top_left: (f32, f32), bot_right: (f32, f32)) -> Self {
+impl<T> QuadTree<T>
+where
+    T: Position,
+{
+    pub fn new(
+        max_nodes: usize,
+        min_size: f32,
+        top_left: (f32, f32),
+        bot_right: (f32, f32),
+    ) -> Self {
         Self {
             nodes: HashMap::new(),
             handle_counter: 0,
-            min_size: 1.0,
-            tree: QuadTreeInner::new(max_nodes, top_left, bot_right),
+            tree: QuadTreeInner::new(max_nodes, min_size, top_left, bot_right),
         }
     }
 
     pub fn insert(&mut self, item: T, pos: (f32, f32)) -> Result<(), QuadTreeInsertError> {
-        if self.tree.insert(pos, self.handle_counter).is_ok() {
-            self.nodes.insert(self.handle_counter, item);
-            self.handle_counter += 1;
+        let mut splits = self.tree.insert(pos, self.handle_counter)?;
 
-            return Ok(());
+        self.nodes.insert(self.handle_counter, item);
+        self.handle_counter += 1;
+
+        while let Some(id) = splits.pop() {
+            let mut new_splits = self
+                .tree
+                .insert(self.nodes.get(&id).unwrap().position(), id)?;
+            splits.append(&mut new_splits);
         }
 
-        Err(QuadTreeInsertError)
+        Ok(())
     }
 
     pub fn lines(&self) -> Vec<((f32, f32), (f32, f32))> {
@@ -62,6 +79,8 @@ struct QuadTreeInner {
     nodes: Vec<u64>,
     max_nodes: usize,
 
+    min_size: f32,
+
     top_left: (f32, f32),
     bot_right: (f32, f32),
 
@@ -69,26 +88,107 @@ struct QuadTreeInner {
 }
 
 impl QuadTreeInner {
-    fn new(max_nodes: usize, top_left: (f32, f32), bot_right: (f32, f32)) -> Self {
+    fn new(max_nodes: usize, min_size: f32, top_left: (f32, f32), bot_right: (f32, f32)) -> Self {
         Self {
             nodes: Vec::new(),
             max_nodes,
+            min_size,
             top_left,
             bot_right,
             quads: None,
         }
     }
 
-    fn insert(&mut self, pos: (f32, f32), handle: u64) -> Result<(), QuadTreeInsertError> {
-        todo!()
+    fn insert(&mut self, pos: (f32, f32), handle: u64) -> Result<Vec<u64>, QuadTreeInsertError> {
+        if !self.in_boundary(pos) {
+            return Err(QuadTreeInsertError);
+        }
+
+        if self.quads.is_none() {
+            if self.nodes.len() < self.max_nodes
+                || (self.top_left.0 - self.bot_right.0).abs() <= self.min_size
+                || (self.top_left.1 - self.bot_right.1).abs() <= self.min_size
+            {
+                self.nodes.push(handle);
+                return Ok(Vec::new());
+            } else {
+                return Ok(self.split());
+            }
+        }
+
+        // quads is now always Some
+        if (self.top_left.0 + self.bot_right.0) / 2.0 >= pos.0 {
+            if (self.top_left.1 + self.bot_right.1) / 2.0 >= pos.1 {
+                // Insert top left
+                self.quads.as_mut().unwrap()[0].insert(pos, handle)?;
+            } else {
+                // insert bot left
+                self.quads.as_mut().unwrap()[1].insert(pos, handle)?;
+            }
+        } else if (self.top_left.1 + self.bot_right.1) / 2.0 >= pos.1 {
+            // Insert top right
+            self.quads.as_mut().unwrap()[2].insert(pos, handle)?;
+        } else {
+            // Insert bot right
+            self.quads.as_mut().unwrap()[3].insert(pos, handle)?;
+        }
+
+        Ok(Vec::new())
     }
 
-    fn in_boundary(&self, pos: (f32, f32)) {
-        todo!()
+    fn in_boundary(&self, pos: (f32, f32)) -> bool {
+        pos.0 >= self.top_left.0
+            && pos.0 < self.bot_right.0
+            && pos.1 >= self.top_left.1
+            && pos.1 < self.bot_right.1
     }
 
-    fn split(&mut self) {
-        todo!()
+    fn split(&mut self) -> Vec<u64> {
+        let top_left_quad = Self::new(
+            self.max_nodes,
+            self.min_size,
+            self.top_left,
+            (
+                (self.top_left.0 + self.bot_right.0) / 2.0,
+                (self.top_left.1 + self.bot_right.1) / 2.0,
+            ),
+        );
+
+        let top_right_quad = Self::new(
+            self.max_nodes,
+            self.min_size,
+            ((self.top_left.0 + self.bot_right.0) / 2.0, self.top_left.1),
+            (
+                (self.top_left.0 + self.bot_right.0) / 2.0,
+                (self.top_left.1 + self.bot_right.1) / 2.0,
+            ),
+        );
+
+        let bot_left_quad = Self::new(
+            self.max_nodes,
+            self.min_size,
+            (self.top_left.0, (self.top_left.1 + self.bot_right.1) / 2.0),
+            ((self.top_left.0 + self.bot_right.0) / 2.0, self.bot_right.1),
+        );
+
+        let bot_right_quad = Self::new(
+            self.max_nodes,
+            self.min_size,
+            (
+                (self.top_left.0 + self.bot_right.0) / 2.0,
+                (self.top_left.1 + self.bot_right.1) / 2.0,
+            ),
+            self.bot_right,
+        );
+
+        self.quads = Some(Box::new([
+            top_left_quad,
+            top_right_quad,
+            bot_left_quad,
+            bot_right_quad,
+        ]));
+
+        self.nodes.drain(..).collect()
     }
 
     fn search_radius(&self, pos: (f32, f32), r: f32) -> Vec<u64> {
